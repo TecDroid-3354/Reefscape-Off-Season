05cef6b39a8f4e4499ffe7ddfce42a2e893e11c0
Pandanator <a01712590@tec.mx>
2025-09-20 19:09:57 -0600

    Added Si un CANRange lateral detecta el coral, un set de rollers gira más para que entre bien, si entra del otro lado pues el otro roller gira más

--- src/main/java/net/tecdroid/subsystems/intake/Intake.kt
+++ src/main/java/net/tecdroid/subsystems/intake/Intake.kt
@@ -2,7 +2,6 @@ package net.tecdroid.subsystems.intake
 
 import com.ctre.phoenix6.configs.CANrangeConfiguration
 import com.ctre.phoenix6.configs.TalonFXConfiguration
-import com.ctre.phoenix6.controls.Follower
 import com.ctre.phoenix6.controls.VoltageOut
 import com.ctre.phoenix6.hardware.CANrange
 import com.ctre.phoenix6.hardware.TalonFX
@@ -12,14 +11,13 @@ import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.AngularVelocity
 import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.button.Trigger
 import net.tecdroid.subsystems.util.generic.TdSubsystem
 import net.tecdroid.util.inches
 import net.tecdroid.util.volts
 
-class Intake(private val coralSensors : List<CANrange>) : TdSubsystem("Intake") {
+class Intake(private val coralSensors: List<CANrange>) : TdSubsystem("Intake") {
     private val config = intakeConfig
     private val algaeMotorController = TalonFX(config.algaeMotorControllerId.id)
     private val coralRightMotorController = TalonFX(config.coralRightMotorControllerId.id)
@@ -39,33 +37,48 @@ class Intake(private val coralSensors : List<CANrange>) : TdSubsystem("Intake") 
         get() = algaeMotorController.get()
 
     init {
+        require( coralSensors.size == 3 )
         configureMotorInterface()
         configureCanRangesInterface()
-        (coralTrigger).and { DriverStation.isTeleop() }.onTrue(InstantCommand({ setCoralVoltage(0.0.volts) }))
+
+        coralTrigger.and { DriverStation.isTeleop() }
+            .onTrue(InstantCommand({ setCoralVoltage(0.0.volts) }))
     }
 
     /**
-     * Sets voltage to the CORAL roller motors.
+     * Sets voltage to the CORAL roller motors dynamically based on lateral CANRange sensors.
      */
-    fun setCoralVoltage(voltage: Voltage) {
-        val request = VoltageOut(voltage)
-        coralRightMotorController.setControl(request)
+    fun setCoralVoltage(baseVoltage: Voltage) {
+        var leftVoltage = baseVoltage
+        var rightVoltage = baseVoltage
+
+        val leftSensor = coralSensors[0]
+        val centerSensor = coralSensors[1]
+        val rightSensor = coralSensors[2]
+
+        when {
+            centerSensor.isDetected.value -> {}
+            leftSensor.isDetected.value -> { leftVoltage += 2.0.volts }
+            rightSensor.isDetected.value -> { rightVoltage += 2.0.volts }
+        }
+
+        coralLeftMotorController.setControl(VoltageOut(leftVoltage))
+        coralRightMotorController.setControl(VoltageOut(rightVoltage))
     }
+
 
     /**
      * Sets voltage to the ALGAE rollers motor.
-     * Same as [setVoltage], another function for the mere convenience of the name.
      */
     fun setAlgaeVoltage(voltage: Voltage) {
         setVoltage(voltage)
     }
 
     /**
-     * Sets voltage to the ALGAE rollers, NOT CORALS.
+     * Sets voltage to the ALGAE rollers only.
      */
     override fun setVoltage(voltage: Voltage) {
-        val request = VoltageOut(voltage)
-        algaeMotorController.setControl(request)
+        algaeMotorController.setControl(VoltageOut(voltage))
     }
 
     fun hasCoral(): Boolean {
@@ -76,46 +89,36 @@ class Intake(private val coralSensors : List<CANrange>) : TdSubsystem("Intake") 
     }
 
     /**
-     * Configures motors for both Coral & Algae Rollers.
-     * Sets the follower request for the left coral motor (right coral motor as lead).
+     * Configures motors for both Coral & Algae Rollers independently.
      */
     private fun configureMotorInterface() {
         val talonConfig = TalonFXConfiguration()
 
         with(talonConfig) {
-            MotorOutput
-                .withNeutralMode(NeutralModeValue.Brake)
-
-            CurrentLimits
-                .withSupplyCurrentLimitEnable(true)
+            MotorOutput.withNeutralMode(NeutralModeValue.Brake)
+            CurrentLimits.withSupplyCurrentLimitEnable(true)
                 .withSupplyCurrentLimit(config.motorsCurrentLimit)
         }
-
 
         algaeMotorController.clearStickyFaults()
         algaeMotorController.configurator.apply(talonConfig)
         coralRightMotorController.configurator.apply(talonConfig)
         coralLeftMotorController.configurator.apply(talonConfig)
 
-        coralLeftMotorController.setControl(Follower(coralRightMotorController.deviceID, true))
+        // No follower: cada motor puede recibir voltaje distinto
     }
 
     /**
-     * Configures each CANRange taking into account the [com.ctre.phoenix6.configs.ProximityParamsConfigs]
-     * and [com.ctre.phoenix6.configs.ToFParamsConfigs]. [com.ctre.phoenix6.configs.FovParamsConfigs] are
-     * not taken into account since the object to detect is significantly bigger than the sensor, they should
-     * not have problems with their field of view.
+     * Configures CANRange sensors.
      */
     fun configureCanRangesInterface() {
         val canRangeConfig = CANrangeConfiguration()
 
-        with (canRangeConfig) {
-            ProximityParams
-                .withProximityThreshold(3.5.inches)
+        with(canRangeConfig) {
+            ProximityParams.withProximityThreshold(3.5.inches)
                 .withProximityHysteresis(0.2.inches)
 
-            ToFParams
-                .withUpdateMode(UpdateModeValue.ShortRange100Hz)
+            ToFParams.withUpdateMode(UpdateModeValue.ShortRange100Hz)
         }
 
         for (sensor in coralSensors) {
