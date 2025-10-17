51913d0a15f952d11f3b0535b2acf565fadacef8
JPablo <A01713288@tec.mx>
2025-10-17 11:26:51 -0600

    Modified both left autos to meet strategy

--- src/main/java/net/tecdroid/autonomous/PathPlannerAutonomous.kt
+++ src/main/java/net/tecdroid/autonomous/PathPlannerAutonomous.kt
@@ -18,6 +18,8 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand
 import net.tecdroid.subsystems.drivetrain.Drive
 import net.tecdroid.systems.ArmSystem.ArmOrders
@@ -126,7 +128,7 @@ class PathPlannerAutonomous(val drive: Drive, private val llController: Limeligh
                 ParallelCommandGroup(
                     Commands.runOnce({ armSystem.disableCoralIntake().schedule() }),
                     Commands.runOnce({ armSystem.disableAlgaeIntake().schedule() })),
-                armSystem.setPoseCommand(ArmPoses.Passive, ArmOrders.EJW.order)
+                armSystem.setPoseCommand(ArmPoses.BackL3, ArmOrders.EJW.order)
             ),
         )
 
@@ -167,7 +169,7 @@ class PathPlannerAutonomous(val drive: Drive, private val llController: Limeligh
                 ParallelCommandGroup(
                     Commands.runOnce({ armSystem.disableCoralIntake().schedule() }),
                     Commands.runOnce({ armSystem.disableAlgaeIntake().schedule() })),
-                armSystem.setPoseCommand(ArmPoses.Passive, ArmOrders.EJW.order)),
+                armSystem.setPoseCommand(ArmPoses.BackL3, ArmOrders.EJW.order)),
             )
 
         registerNamedCommand("L3-AlignScoreLeftBranch",
@@ -200,7 +202,7 @@ class PathPlannerAutonomous(val drive: Drive, private val llController: Limeligh
                 ParallelCommandGroup(
                     Commands.runOnce({ armSystem.disableCoralIntake().schedule() }),
                     Commands.runOnce({ armSystem.disableAlgaeIntake().schedule() })),
-                armSystem.setPoseCommand(PoseCommands.Passive)
+                WaitCommand(0.1.seconds)
             )
             )
     }
