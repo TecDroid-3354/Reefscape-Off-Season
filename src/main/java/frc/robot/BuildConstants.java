5976a25f3c7318d6f9814dbf8639737d37c9c892
Melvin Matamoros <matamorosmelvin82@gmail.com>
2025-10-31 08:08:24 -0600

    Ignore this commit, is for the collabs' lectures

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape-Off-Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 355;
-  public static final String GIT_SHA = "d598e2ac3879bd74089b5a1dfc2df588c99fe5a2";
-  public static final String GIT_DATE = "2025-10-16 12:34:10 CST";
+  public static final int GIT_REVISION = 359;
+  public static final String GIT_SHA = "964f15315e7508834a949f71ef976ba8967ced0f";
+  public static final String GIT_DATE = "2025-10-17 12:26:41 CST";
   public static final String GIT_BRANCH = "position-record";
-  public static final String BUILD_DATE = "2025-10-16 17:36:02 CST";
-  public static final long BUILD_UNIX_TIME = 1760657762162L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-10-29 15:55:19 CST";
+  public static final long BUILD_UNIX_TIME = 1761774919090L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
